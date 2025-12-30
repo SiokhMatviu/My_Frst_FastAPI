27b16c62eba45651b1ea41e9c4f40ffb82e9660e
SiokhMatviu <siokhmatviu@gmail.com>
2025-12-30 17:04:52 +0200

    initial commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from database import create_tables, delete_tables
 from router import router as router_tasks
+from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
@@ -14,6 +15,22 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(router_tasks)
+
+
+origins = [
+    "http://localhost:5173",  # Vite
+    "http://127.0.0.1:5173",
+    "https://your-frontend-domain.com",  # якщо буде прод
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,        # або ["*"] для тестів
+    allow_credentials=True,
+    allow_methods=["*"],          # GET, POST, PUT, DELETE
+    allow_headers=["*"],          # Authorization, Content-Type
+)
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
