12e24a99663e113da9a9f8b173520e4ae1f6892c
SiokhMatviu <siokhmatviu@gmail.com>
2025-12-30 10:36:48 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import uvicorn
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from database import create_tables, delete_tables
+from router import router as router_tasks
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    await create_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router_tasks)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
